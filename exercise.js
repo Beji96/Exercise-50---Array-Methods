6cc14f20e110a3e6db0fe3a02561e3e1f2d1a36e
Flavius <flaviusbejan96@yahoo.com>
2022-09-22 10:49:58 +0300

    finish

---
+++ exercise.js
@@ -0,0 +1,9 @@
+const names = ["Luca", "Marco", "Vittorio", "Giovanni"];
+names.pop();
+console.log(names);
+
+names.push("Pippo");
+console.log(names);
+
+names.unshift("Giovanni");
+console.log(names);
